1e6bb62797bce0b965cd65bdea74cb5c01ef2584
Sergey Tetenko <80429666+Nalfeun@users.noreply.github.com>
2022-02-22 22:46:19 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,91 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector(".gallery");
+const imgMarkup = createImgCard(galleryItems);
+
+imgContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createImgCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    })
+    .join("");
+}
+
+let currentIndex = 0;
+
+const options = {
+  onShow: (instance) => {
+    window.addEventListener("keydown", pressEsc);
+  },
+
+  onClose: (instance) => {
+    window.removeEventListener("keydown", pressEsc);
+  },
+};
+const instance = basicLightbox.create(
+  `<img src="" width="800" height="600">`,
+  options
+);
+
+imgContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let imgLink = e.target.getAttribute("data-source");
+
+  originalLink(imgLink);
+  currentIndex = Number(e.target.getAttribute("data-index"));
+  instance.show();
+});
+
+function originalLink(url) {
+  instance.element().querySelector("img").src = url;
+}
+
+function pressEsc({ key }) {
+  switch (key) {
+    case "ArrowRight":
+      currentIndex += 1;
+      if (currentIndex >= galleryItems.length) {
+        currentIndex = 0;
+      }
+
+      originalLink(galleryItems[currentIndex].original);
+      break;
+
+    case "ArrowLeft":
+      currentIndex -= 1;
+      if (currentIndex <= 0) {
+        currentIndex = galleryItems.length - 1;
+      }
+
+      originalLink(galleryItems[currentIndex].original);
+      break;
+
+    case "Escape":
+      instance.close();
+      break;
+
+    default:
+      break;
+  }
+}
